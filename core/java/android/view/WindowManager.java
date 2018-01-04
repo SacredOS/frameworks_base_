OctoDev-Project-Legacy/octogen_frameworks_base

FWB: Squashed commit for Expanded Desktop [1/2]
Bringup expanded desktop and implement per-user configuration.

Change-Id: I90d834d788b3050a3f4c11dda9b6d1a7d79546cc

Create configurable default expanded desktop style.

@mydongistiny: updated for oreo
Change-Id: I90c7f8544cda9b91fb9b700fcf146f4e58b15a26
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

Signed-off-by: XrTV <binder_z@mail.ru>

core/java/android/view/WindowManager.java
@@ -1391,6 +1391,23 @@ public interface WindowManager extends ViewManager {
         public static final int PRIVATE_FLAG_TASK_SNAPSHOT = 0x00080000;
 
         /**
+         * {@hide}
+         */
+        public static final int PRIVATE_FLAG_STATUS_HIDE_FORCED = 0x01000000;
+
+        /**
+         * {@hide}
+         */
+        public static final int PRIVATE_FLAG_NAV_HIDE_FORCED = 0x02000000;
+
+        /**
+         * The window had not set FULLSCREEN flag so don't handle it as fullscreen in layoutWindowLw
+         *
+         * {@hide}
+         */
+        public static final int PRIVATE_FLAG_WAS_NOT_FULLSCREEN = 0x03000000;
+
+        /**
          * Control flags that are private to the platform.
          * @hide
          */

SacredOS/frameworks_base_

core/java/android/view/WindowManager.java
@@ -1434,6 +1434,23 @@ public interface WindowManager extends ViewManager {
         public static final int PRIVATE_FLAG_ACQUIRES_SLEEP_TOKEN = 0x00200000;
 
         /**
+         * {@hide}
+         */
+        public static final int PRIVATE_FLAG_STATUS_HIDE_FORCED = 0x05000000;
+
+        /**
+         * {@hide}
+         */
+        public static final int PRIVATE_FLAG_NAV_HIDE_FORCED = 0x06000000;
+
+        /**
+         * The window had not set FULLSCREEN flag so don't handle it as fullscreen in layoutWindowLw
+         *
+         * {@hide}
+         */
+        public static final int PRIVATE_FLAG_WAS_NOT_FULLSCREEN = 0x07000000;
+
+        /**
          * Control flags that are private to the platform.
          * @hide
          */