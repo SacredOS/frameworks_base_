a73a7bc05581e15dc38948975a9adc97fc2d2fc9
Altaf-Mahdi <altaf.mahdi@gmail.com>
2018-01-14 07:49:02 -0500

    Keyguard/Statusbar Carrier label options
    * Stock behavior(only on keyguard), only on statusbar,both disabled or both enabled
    
    PureNexus Edits:
    * adapted to replace the carrier label toggle in PureNexus-OLD/android_frameworks_base@16a247a
    * this now provides more robust options for carrier label customization
    
    AICP brought up to O by AliB:
    - Adapted to O
    
    Change-Id: I6a73a15160c8334f90feb3a26906a13071097ed7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4578,8 +4578,12 @@ public final class Settings {
         public static final String SCREENRECORD_QUALITY_MODE = "screenrecord_quality_mode";
 
         /**
-         * show carrier in statusbar. The value is
-         * int (1: Show or 0: Hide).
+         * Status bar carrier label
+         * 0: Hide
+         * 1: Display on keyguard status bar
+         * 2: Display on Normal status bar
+         * 3: Enabled for both
+         * @hide
          */
         public static final String STATUS_BAR_CARRIER = "status_bar_carrier";
 
