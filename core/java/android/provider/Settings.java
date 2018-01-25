428a2e6cfdac2aa4e47e638740565fa051c0d635
therootlord <igor_cestari@hotmail.com>
2018-01-25 03:06:55 +0000

    Revert "Disable dashboard suggestions (1/2)"
    This reverts commit 44b4eff89be9d45befa3c0da00422895d41e4215.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4047,12 +4047,6 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
 
         /**
-         * Disable dashboard suggestions in settings
-         * @hide
-         */
-        public static final String ENABLE_SUGGESTIONS = "enable_suggestions";
-
-        /**
          * Whether the phone vibrates on call connect
          * @hide
          */
@@ -4069,7 +4063,6 @@ public final class Settings {
          * @hide
          */
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
-
 
         /**
          * Disable dashboard conditions in settings
