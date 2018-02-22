1e6056dd587cfa98f65dd8edc23d7cea2bffa430
therootlord <igor_cestari@hotmail.com>
2018-02-22 02:05:25 +0000

    Revert "[1/2] base: poor man battery style tweaks"
    This reverts commit 9e8dd2424971b77a149843f37bb1311855b8a06b.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4266,11 +4266,6 @@ public final class Settings {
 
 
         /**
-         * @hide
-         */
-        public static final String SHOW_BATTERY_IMAGE = "status_bar_show_battery_image";
-
-        /**
          * Volume key controls ringtone or media sound stream
          * @hide
          */
